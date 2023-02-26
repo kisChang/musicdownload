077f4a2c56aab11e9c17207013550e4dc052f326
KisChang <734615869@qq.com>
2023-02-26 17:43:28 +0800

    update GUI

--- main.py
+++ main.py
@@ -12,48 +12,43 @@ class GUI:
         master.title("全网音乐下载器")
 
         # 创建三个区域
-        self.top_frame = tk.Frame(master)
-        self.middle_frame = tk.Frame(master)
-        self.bottom_frame = tk.Frame(master)
+        self.top_frame = tk.Frame(master, padx=10, pady=10)
+        self.middle_frame = tk.Frame(master, padx=10, pady=10)
+        self.bottom_frame = tk.Frame(master, padx=10, pady=10)
 
         # 顶部区域
-        self.drives = win32api.GetLogicalDriveStrings() # 获取所有盘符
+        self.drives = win32api.GetLogicalDriveStrings()  # 获取所有盘符
         self.drives = self.drives.split('\000')[:-1]
-        self.location_label = tk.Label(self.top_frame, text="选择存储位置：")
+        self.location_label = tk.Label(self.top_frame, text="选择存储位置：", font=('楷体', 18))
         self.location_label.pack(side=tk.LEFT)
         self.location_var = tk.StringVar()
         self.location_var.set(self.drives[0])
         for drive in self.drives:
             drive_radio = tk.Radiobutton(self.top_frame, text=drive, variable=self.location_var, value=drive)
-            drive_radio.pack(side=tk.LEFT)
+            drive_radio.pack(side=tk.LEFT, padx=5, pady=5)
 
         # 中间区域
         # 设置窗口大小以及出现的位置
-        self.label = tk.Label(self.middle_frame, text="请输入下载的歌曲:", font=('楷体', 20))
-        self.label.grid(row=0)
+        self.label = tk.Label(self.middle_frame, text="请输入下载的歌曲", font=('楷体', 18))
+        self.label.grid(row=0, columnspan=2, pady=5)
         self.addr = tk.StringVar(value='')
-        self.entry = tk.Entry(self.middle_frame, font=('宋体', 20), textvariable=self.addr)
-        self.entry.grid(row=0, column=1)
+        self.entry = tk.Entry(self.middle_frame, font=('宋体', 20), textvariable=self.addr, width=25)
+        self.entry.grid(row=1, column=0, padx=5, pady=5)
         # 单选按钮
         self.var = tk.StringVar(value='netease')
-        self.r1 = tk.Radiobutton(self.middle_frame, text='网易云', variable=self.addr, value='netease')
-        self.r1.grid(row=1, column=0)
-        self.r2 = tk.Radiobutton(self.middle_frame, text='QQ', variable=self.addr, value='qq')
-        self.r2.grid(row=1, column=1)
-        # 列表框
-        self.text = tk.Listbox(self.middle_frame, font=('楷体', 16), width=50, height=15)
-        self.text.grid(row=2, columnspan=2)
+        self.r1 = tk.Radiobutton(self.middle_frame, text='网易云', variable=self.var, value='netease')
+        self.r1.grid(row=1, column=1, padx=5, pady=5)
+        self.r2 = tk.Radiobutton(self.middle_frame, text='QQ', variable=self.var, value='qq')
+        self.r2.grid(row=1, column=2, padx=5, pady=5)
         # 下载按钮
         self.button1 = tk.Button(self.middle_frame, text='开始下载', font=('楷体', 15), command=self.get_music_name)
-        self.button1.grid(row=3, column=0)
+        self.button1.grid(row=3, column=1, padx=5, pady=5, sticky='w')
         self.button2 = tk.Button(self.middle_frame, text='退出程序', font=('楷体', 15), command=root.quit)
-        self.button2.grid(row=3, column=1)
+        self.button2.grid(row=3, column=2, padx=5, pady=5, sticky='e')
 
         # 底部区域
-        self.message_label = tk.Label(self.bottom_frame, text="提示信息：")
-        self.message_label.pack(side=tk.LEFT)
-        self.message_text = tk.Text(self.bottom_frame, width=50, height=5)
-        self.message_text.pack(side=tk.LEFT)
+        self.text = tk.Listbox(self.bottom_frame, font=('楷体', 16), width=50, height=15)
+        self.text.grid(row=2, columnspan=3, padx=5, pady=5)
 
         # 整合三个区域
         self.top_frame.pack()
@@ -110,5 +105,5 @@ class GUI:
 if __name__ == '__main__':
     root = tk.Tk()
     gui = GUI(root)
-    root.geometry('560x450+400+200')
+    root.geometry('600x600+400+200')
     root.mainloop()
